8019496d3c711690214ca01ac8ed8379a1b87e89
MrFox <st@163.com>
2024-07-22 23:53:02 +0800

    feat：新增动态增强功能测试

---
+++ jagent-javasisit/src/main/java/com/lqx/fort/JavaJST.java
@@ -0,0 +1,29 @@
+package com.lqx.fort;
+
+import com.sun.tools.javac.parser.Scanner;
+import com.sun.tools.javac.parser.ScannerFactory;
+import com.sun.tools.javac.parser.Tokens;
+import com.sun.tools.javac.util.Context;
+
+public class JavaJST {
+    public static void main(String[] args) {
+        ScannerFactory instance = ScannerFactory.instance(new Context());
+        Scanner scanner = instance.newScanner("System.out.println(\"Hello, world!\");", false);
+
+        while (true) {
+            scanner.nextToken();
+
+            Tokens.Token token = scanner.token();
+
+            String tok = null;
+            try {
+                tok = token.name().toString();
+            }catch (Exception e){
+            }
+            System.out.println(tok != null? tok : token.kind.toString());
+            if(token.kind == Tokens.TokenKind.EOF){
+                break;
+            }
+        }
+    }
+}
